43e2620e98f67417c8e1f46835a53156d344cb7e
annoeul <shdmf1137@gmail.com>
2023-06-02 23:02:49 +0900

    Design: UI변경

--- src/components/user/register/index.jsx
+++ src/components/user/register/index.jsx
@@ -8,7 +8,7 @@ function RegisterForm() {
   return (
     <Container component="main" maxWidth="sm">
       <Box sx={{ marginTop: 5, display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" variant="h5" mb={1}>
           회원 정보 등록
         </Typography>
         <Grid container>
@@ -16,11 +16,12 @@ function RegisterForm() {
             <TextField name="email" label="이메일" required fullWidth autoComplete="email" margin="normal" autoFocus />
           </Grid>
           <Grid item xs={2}>
-            <Button type="submit" variant="contained" sx={{ mt: 2, mb: 2 }} style={{ height: "55px" }}>
+            <Button type="submit" variant="contained" sx={{ mt: 2, mb: 2, ml: 1 }} style={{ height: "55px" }}>
               중복확인
             </Button>
           </Grid>
         </Grid>
+
         <TextField
           name="password"
           label="비밀번호"
@@ -39,16 +40,17 @@ function RegisterForm() {
           autoComplete="current-password"
           margin="normal"
         />
+        <TextField name="call" label="휴대폰 번호" type="text" required fullWidth margin="normal" />
 
         <hr style={{ width: "100%", margin: "30px 0", border: "dashed", borderWidth: ".1px", color: "grey" }} />
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" variant="h5" mb={1}>
           가게 정보 등록
         </Typography>
 
-        <TextField name="storenumber" label="사업자 등록 번호" type="text" required fullWidth margin="dense" />
-        <TextField name="name" label="창업자명" required fullWidth margin="normal" />
-
-        <TextField name="date" label="창업일(ex:20230430)" type="text" required fullWidth margin="normal" />
+        <TextField name="storeName" label="가게명" type="text" required fullWidth margin="normal" />
+        <TextField name="ownerName" label="사업주명" type="text" required fullWidth margin="normal" />
+        <TextField name="address" label="주소" type="text" required fullWidth margin="normal" />
+        <TextField name="crn" label="사업자등록번호" type="text" required fullWidth margin="normal" />
 
         <Button type="button" fullWidth variant="contained" sx={{ mt: 1, mb: 2 }} style={{ height: "50px" }}>
           사업자 정보 인증
