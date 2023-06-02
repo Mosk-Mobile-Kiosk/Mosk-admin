1edc8c5ab0e677d35585a05408708823c62432d5
annoeul <shdmf1137@gmail.com>
2023-06-02 22:53:30 +0900

    Design: 회원가입 페이지 하나로 합침

--- src/components/user/register/index.jsx
+++ src/components/user/register/index.jsx
@@ -1,16 +1,15 @@
 import React from "react"
-import { Box, Button, Container, FormControlLabel, Grid, TextField, Typography } from "@mui/material"
-// import { CheckBox } from "@mui/icons-material"
-import { Link, useNavigate } from "react-router-dom"
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
+import { Box, Button, Container, Grid, TextField, Typography } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 function RegisterForm() {
   const useNav = useNavigate()
+
   return (
     <Container component="main" maxWidth="sm">
       <Box sx={{ marginTop: 5, display: "flex", flexDirection: "column", alignItems: "center" }}>
         <Typography component="h1" variant="h5">
-          사용자 정보 등록
+          회원 정보 등록
         </Typography>
         <Grid container>
           <Grid item xs={10}>
@@ -28,7 +27,7 @@ function RegisterForm() {
           type="password"
           required
           fullWidth
-          autoComplete="current-password"
+          autoComplete="password"
           margin="dense"
         />
         <TextField
@@ -41,10 +40,21 @@ function RegisterForm() {
           margin="normal"
         />
 
-        <Button type="button" fullWidth variant="contained" sx={{ mt: 2, mb: 2 }} style={{ height: "50px" }}>
-          <Link to="/storeRegister" style={{ color: "white" }}>
-            다음단계 (가게 정보 등록)
-          </Link>
+        <hr style={{ width: "100%", margin: "30px 0", border: "dashed", borderWidth: ".1px", color: "grey" }} />
+        <Typography component="h1" variant="h5">
+          가게 정보 등록
+        </Typography>
+
+        <TextField name="storenumber" label="사업자 등록 번호" type="text" required fullWidth margin="dense" />
+        <TextField name="name" label="창업자명" required fullWidth margin="normal" />
+
+        <TextField name="date" label="창업일(ex:20230430)" type="text" required fullWidth margin="normal" />
+
+        <Button type="button" fullWidth variant="contained" sx={{ mt: 1, mb: 2 }} style={{ height: "50px" }}>
+          사업자 정보 인증
+        </Button>
+        <Button type="button" fullWidth variant="contained" sx={{ mb: 2 }} style={{ height: "50px" }}>
+          가입 완료!
         </Button>
       </Box>
     </Container>
