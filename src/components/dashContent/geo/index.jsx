2bef5ab7573c603fc3d4d3cbbe7183f8320caa9b
annoeul <shdmf1137@gmail.com>
2023-06-14 02:21:42 +0900

    Feat: QR Code 생성

--- src/components/dashContent/geo/index.jsx
+++ src/components/dashContent/geo/index.jsx
@@ -1,15 +1,36 @@
-import { Box } from "@mui/material"
-import Header from "../../common/header/Header"
-import GeoChart from "../../common/geochart/index"
+import React, { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Geo = () => {
+  const nav = useNavigate()
+  const [qrCodeImage, setQRCodeImage] = useState(null)
+
+  const getQRCode = () => {
+    fetch("http://localhost:9090/api/v1/stores/qrcode", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + sessionStorage.getItem("accessToken"),
+      },
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        const imageURL = URL.createObjectURL(blob)
+        setQRCodeImage(imageURL)
+      })
+      .catch((error) => {
+        console.error("Error:", error)
+      })
+  }
+
+  useEffect(() => {
+    getQRCode()
+  }, [])
+
   return (
-    <Box m="20px">
-      <Header title="Geo Chart" subtitle="Simple Geo Chart" />
-      <Box height="75vh">
-        <GeoChart />
-      </Box>
-    </Box>
+    <div>
+      <button onClick={getQRCode}>Get QR Code</button>
+      {qrCodeImage && <img src={qrCodeImage} alt="QR Code" />}
+    </div>
   )
 }
 
